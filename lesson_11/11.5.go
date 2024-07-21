be7fc560f8e767002becd048f5cc5b72e7277276
Konstantin <mkv2008@yandex.ru>
2024-07-21 16:12:01 +0300

    добавил вариант к задаче 11.5

--- lesson_11/11.5.go
+++ lesson_11/11.5.go
@@ -15,13 +15,23 @@ func main() {
 
 	for i := 0; i < len(a); i++ {
 
-		switch a[i] % 2 {
+		/*switch a[i] % 2 {
 
 		case 0:
 			fmt.Println(a[i], " чётное")
 		default:
+			fmt.Println(a[i], " не чётное")	//  Что с "o" делать без "if", пока не понимаю
+		}*/
+		switch 0 {
+
+		case a[i]:
+			fmt.Println(a[i], " равно 0")
+		case a[i] % 2:
+			fmt.Println(a[i], " чётное")
+		default:
 			fmt.Println(a[i], " не чётное")
-		}
-	} //  Что с "o" делать без "if", пока не понимаю
+		} // xчто-то придумал
+
+	}
 
 }
--- lesson_12/12.1.go
+++ lesson_12/12.1.go
@@ -2,4 +2,28 @@
 возвращает ошибку. Вызовите эту функцию и обработайте возвращенную
 ошибку. */
 
+package main
 
+import (
+	"fmt"
+	"log"
+	"strconv"
+)
+
+func main() {
+
+	m := []int{2, 3, 7}
+
+	c := "56r7"
+
+	i, err := strconv.Atoi(c)
+	if err != nil {
+		fmt.Errorf("kbhsb : %d", i)
+		log.Fatal(err)
+		return
+	}
+
+	m[0] = i
+
+	fmt.Println(m)
+}
