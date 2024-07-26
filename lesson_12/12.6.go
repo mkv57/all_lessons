7c7426caa4a7ffcd3d13c03b3aa116c00739b776
Konstantin <mkv2008@yandex.ru>
2024-07-26 11:45:14 +0300

    все задания по ошибкам готовы

--- lesson_12/12.6.go
+++ lesson_12/12.6.go
@@ -2,3 +2,34 @@
 собственный тип ошибки, реализовав интерфейс error. Создайте функцию,
 которая возвращает вашу кастомную ошибку, и обработайте её. */
 
+package main
+
+import (
+	"fmt"
+)
+
+var r = MyError{"не правильно введено число"}
+
+func main() {
+
+	r := MyError{"не правильно введено число"}
+	err := Error()
+	if err != nil {
+		fmt.Println(r)
+
+	}
+
+}
+
+type MyError struct {
+	Msg string
+}
+
+func Error() error {
+	var c int
+
+	_, err := fmt.Scan(&c)
+
+	return err
+
+}
